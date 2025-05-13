b7f272fee5040c41f81ba3b6b735bd3c19ee04d1
Tejas <ttejas220@gmail.com>
2025-05-13 11:54:33 +0530

    convex auth and database schema done

---
+++ app/providers/ConvexClerkProvider.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+  throw new Error("Missing NEXT_PUBLIC_CONVEX_URL in your .env file");
+}
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+
+function ConvexClerkProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
+
+export default ConvexClerkProvider;
---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://proper-beagle-1.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
