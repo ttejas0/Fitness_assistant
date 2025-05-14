59aa0f018880dcb6b441b2027389e8ff21990bb7
Tejas <ttejas220@gmail.com>
2025-05-14 21:37:55 +0530

    Generate program page logic done

--- app/generate-program/page.tsx
+++ app/generate-program/page.tsx
@@ -1,7 +1,120 @@
-import React from "react";
+"use client";
+import { vapi } from "@/lib/vapi";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
 
 const GenerateProgramPage = () => {
-  return <div>GenerateProgramPage</div>;
+  const [callActive, setCallActive] = useState(false);
+  const [connecting, setConnecting] = useState(false);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [callEnded, setCallEnded] = useState(false);
+
+  const { user } = useUser();
+  const router = useRouter();
+
+  const messageContainerRef = useRef<HTMLDivElement>(null);
+
+  // auto-scroll messages
+  useEffect(() => {
+    if (messageContainerRef.current) {
+      messageContainerRef.current.scrollTop =
+        messageContainerRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  //Navigate the user to profile page after the call ends
+  useEffect(() => {
+    if (callEnded) {
+      const redirectTimer = setTimeout(() => {
+        router.push("/profile");
+      }, 1500);
+
+      return () => clearTimeout(redirectTimer);
+    }
+  }, [callEnded, router]);
+
+  // Set up event listeners for vapi
+  useEffect(() => {
+    const handleCallStart = () => {
+      console.log("Call started");
+      setConnecting(false);
+      setCallActive(true);
+      setCallEnded(false);
+    };
+
+    const handleCallEnd = () => {
+      console.log("Call ended");
+      setCallActive(false);
+      setConnecting(false);
+      setIsSpeaking(false);
+      setCallEnded(true);
+    };
+
+    const handleSpeechStart = () => {
+      console.log("AI Speech started");
+      setIsSpeaking(true);
+    };
+
+    const handleSpeechEnd = () => {
+      console.log("AI Speech ended");
+      setIsSpeaking(false);
+    };
+
+    const handleCallMessage = (message: any) => {};
+    const handleError = (error: any) => {
+      console.log("Vapi Error", error);
+      setConnecting(false);
+      setCallActive(false);
+    };
+
+    vapi
+      .on("call-start", handleCallStart)
+      .on("call-end", handleCallEnd)
+      .on("speech-start", handleSpeechStart)
+      .on("speech-end", handleSpeechEnd)
+      .on("message", handleCallMessage)
+      .on("error", handleError);
+
+    // Cleanup event listeners on unmount
+    return () => {
+      vapi
+        .off("call-start", handleCallStart)
+        .off("call-end", handleCallEnd)
+        .off("speech-start", handleSpeechStart)
+        .off("speech-end", handleSpeechEnd)
+        .off("message", handleCallMessage)
+        .off("error", handleError);
+    };
+  }, []);
+
+  const toggleCall = async () => {
+    if (callActive) vapi.stop();
+    else {
+      try {
+        setConnecting(true);
+        setMessages([]);
+        setCallEnded(false);
+
+        const fullName = user?.firstName
+          ? `${user.firstName} ${user.lastName || ""}`.trim()
+          : "There";
+
+        await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
+          variableValues: {
+            full_name: fullName,
+          },
+          clientMessages: [],
+          serverMessages: [],
+        });
+      } catch (error) {
+        console.log("Failed to start call", error);
+      }
+    }
+  };
+
+  return <div></div>;
 };
 
 export default GenerateProgramPage;
