089c77524fdc3fdc08c4e4dd8da3bd39bb20790c
Tejas <ttejas220@gmail.com>
2025-05-18 18:36:34 +0530

    convex user.update to DB

--- convex/http.ts
+++ convex/http.ts
@@ -71,6 +71,29 @@ http.route({
         });
       }
     }
+
+    if (eventType === "user.updated") {
+      const { id, first_name, last_name, image_url, email_addresses } =
+        evt.data;
+
+      const email = email_addresses[0].email_address;
+
+      const name = `${first_name || ""} ${last_name || ""}`.trim();
+
+      try {
+        await ctx.runMutation(api.users.updateUser, {
+          clerkId: id,
+          email,
+          name,
+          image: image_url,
+        });
+      } catch (error) {
+        console.log("Error creating user:", error);
+        return new Response("Error creating user", {
+          status: 500,
+        });
+      }
+    }
     return new Response("webhook processed successfully", {
       status: 200,
     });
--- convex/users.ts
+++ convex/users.ts
@@ -17,3 +17,21 @@ export const syncUser = mutation({
     return await ctx.db.insert("users", args);
   },
 });
+
+export const updateUser = mutation({
+  args: {
+    name: v.string(),
+    email: v.string(),
+    clerkId: v.string(),
+    image: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const existingUser = await ctx.db
+      .query("users")
+      .withIndex("by_clerk_id", (q) => q.eq("clerkId", args.clerkId))
+      .first();
+
+    if (!existingUser) return;
+    return await ctx.db.patch(existingUser._id, args);
+  },
+});
